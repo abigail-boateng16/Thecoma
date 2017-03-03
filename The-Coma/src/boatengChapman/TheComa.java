026543e6297f1a5fb1e41d42d9d4f3ced2369605
abigail-boateng <324213115@C-3ZZHYR1.tdsb.on.ca>
2017-03-03 11:24:30 -0500

    Finished all the paths and added comments

--- The-Coma/src/boatengChapman/TheComa.java
+++ The-Coma/src/boatengChapman/TheComa.java
@@ -5,16 +5,22 @@ public class TheComa {
 
 	public static void main(String[] args) {
 		Console console = new Console();
-		String answer1;
-		String answer2;
-		String answer3;
-		String answer4;
-		String answer5;
-		String answer6;
-		String answer7;
-		String answer8;
-		String answer99;
-		double number1 = Math.random();
+		String answer1;//searching the hospital room or  leaving
+		String answer2;//going to Veronica's room or trying to leave the hospital
+		String answer3;//searching the body in Veronica's room or leave the hospital
+		String answer4;//going to the generator room or doing nothing
+		String answer5;//asking why she's here, trying to leave the hospital or asking what happened
+		String answer6;//ask her why she's been coming back to see you or leaving with her
+		String answer7;//accepting that she killed everyone or trying to leave without her
+		String answer8;//trying to disarm her or trying to escape
+		String answer99;//playing again or not
+		String answer9;//going left or right in the hallway
+		String answer10;//calling the police or calling home
+		String answer11;//waiting for the police or walking around
+		String answer12;//picking up the axe or just standing near it
+		double number1 = Math.random();//Always giving the greater possibility
+		double number2 = Math.random();
+		double number3 = Math.random();
 		do{
 		console.println("Some time ago, you were involved in a terrible car accident on Valentine's Day with your loving wife of 15 years, Veronica. You are just waking up from the resulting coma in a hospital that seems to be abandoned. Would you like to search your room or leave? (search room/leave)");
 		answer1 = console.readLine();
@@ -41,6 +47,18 @@ public class TheComa {
 									if(answer7.equals("accept")){
 										console.println("You leave with her and live with a psychopath for the rest of your life");
 									}
+									else{
+										console.println("You try and convince her to let you leave by yourself.");
+										if (number2 <= 0.5){
+											console.println("She becomes angered by your attempt and swings her axe at you. YOU ARE DEAD");
+										}
+										else{
+											while (number2 >=0.5){
+												console.println("She denies your request and refuses to let you go. Do you accept this and try to leave with her or leave by yourself? (accept/ leave)");
+												answer7 = console.readLine();//if the put accept we need to go back to the if statement, if the put leave, go back to else
+											}
+										}
+									}
 								}
 								
 							}
@@ -48,7 +66,15 @@ public class TheComa {
 								console.println("You leave with Veronica without ever finding out what happened here.");
 							}
 							else{
-								console.println("I killed everyone here to make sure that I could see you every day. Do you try to disarm her or just try to slip by and escape? (disarm/leave");//must continue
+								console.println("I killed everyone here to make sure that I could see you every day. You notice that she is holding an axe. Do you try to disarm her or just try to slip by and escape? (disarm/leave");//must continue
+								answer8 = console.readLine();
+								 if (answer8.equals("disarm")){
+									 console.println("You reach for the axe and successfully disarm her and escape. Although you get out without a scratch, you live the rest of your life alone. THE END");
+								 }
+								 else{
+									 console.println("While trying to slip by, Veronica swings the axe at you and hits you in the back. YOU ARE DEAD");
+								 }
+								 
 							}
 						}
 						else{
@@ -78,9 +104,51 @@ public class TheComa {
 					answer3 = console.readLine();//need to figure out how to get back to normal part of program
 			}
 		}//search room or leave
-		
+			
 
 	}
+		else{
+			console.println("You leave your room and enter a hallway. Do you go left or right? (left/ right)");
+			answer9 = console.readLine();
+			if (answer9.equals("left")){
+				console.println("The ceiling falls in and you die. YOU ARE DEAD");
+				
+			}
+			else{
+				console.println("You find a telephone. Do you try and call the police or call your house and try to reach your wife? (police/ home)");
+				answer10 = console.readLine();
+				if (answer10.equals("home")){
+					console.println("The phone rings and you wait for your wife to answer. After a while no one picks up and you feel a dark presence approaching but as you turn around you see an axe about to swing down on you. YOU ARE DEAD!");
+				}
+				else{
+					console.println("You call the police.");
+					if (number3 >=0.6){
+						console.println("You talk to the police and they tell you that they're on the way. Do you wait near the phone or decide to walk around? (wait/ walk)");
+						answer11 = console.readLine();
+						if (answer11.equals("walk")){
+							console.println("You find an axe in a pool of blood. Do you pick it up or leave it? (pick up/ leave it)");
+							answer12 = console.readLine();
+							if (answer12.equals("pick up")){
+								console.println("You pick up the axe and stand there admiring it until you see a figure approaching you. It is Veronica and she tells you that she killed everyone in the hospital. You're horrified as she walks closer to you with a switch blade, so you swing the axe and kill her. Not long after, the police arrive and see what you've done and arrest you. THE END.");
+								
+							}
+							else{
+								console.println("The police arrive after a few minutes and take you home. THE END");
+							}
+								
+						}
+						else{
+							console.println("You wait around then feel a dark presence approaching but as you turn around you see an axe about to swing down on you. YOU ARE DEAD!");
+						}
+					}
+					else{
+						console.println("The line seems to be busy. Do you call home or try and call the police again? (home/ police)");
+						answer12 = console.readLine();
+					}
+				}
+			}
+		}
+		
 
 	
 console.println("Would you like to play again?");
